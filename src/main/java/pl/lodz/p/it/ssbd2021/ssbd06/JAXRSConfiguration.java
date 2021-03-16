02f39c36f005ffe5a9c4b6c31a39ef7c086eb320
224434 Szcześniak Krzysztof <224434@edu.p.lodz.pl>
2021-03-16 22:39:42 +0100

    Pierwsza wersja projektu

---
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.ssbd2021.ssbd06;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}

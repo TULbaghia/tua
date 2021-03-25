ee74c26e49f94859a0d954a17d3a8661dbb0b3d3
224438 Szewc Mateusz <224438@edu.p.lodz.pl>
2021-03-25 21:01:51 +0100

    Removed unnecessary queries from auth.sql

--- auth.sql
+++ auth.sql
@@ -1,4 +1,2 @@
--- GRANT SELECT ON TABLE "Auth_View" TO ssbd00auth
-
--- SELECT "Password" FROM "Auth_View" WHERE "Login" = ?;
--- SELECT "Role" FROM "Auth_View" WHERE "Login" = ?;
+SELECT "Password" FROM "Auth_View" WHERE "Login" = ?;
+SELECT "Role" FROM "Auth_View" WHERE "Login" = ?;

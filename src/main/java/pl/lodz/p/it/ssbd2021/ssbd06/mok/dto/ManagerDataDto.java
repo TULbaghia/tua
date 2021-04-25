e60eb3b520063e78a7af634d0fd7a20bfda49409
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-04-25 22:06:28 +0200

    Poprawki: Dodanie do klas dziedziczących po RoleDto adnotacji Lomboka: @Data, @NoArgsConstructor oraz @@EqualsAndHashCode(callSuper = true). Dodanie do klas dziedziczących po RoleDto konstruktorów parametrowych.

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/mok/dto/ManagerDataDto.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/mok/dto/ManagerDataDto.java
@@ -1,14 +1,19 @@
 package pl.lodz.p.it.ssbd2021.ssbd06.mok.dto;
 
 import lombok.*;
+import pl.lodz.p.it.ssbd2021.ssbd06.entities.enums.AccessLevel;
 
 /**
  * Klasa DTO dziedzicząca po RoleDto, reprezentująca rolę Manager w systemie
  */
-@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 public class ManagerDataDto extends RoleDto{
     private String hotelName;
+
+    public ManagerDataDto(String login, AccessLevel accessLevel, boolean enabled, String hotelName) {
+        super(login, accessLevel, enabled);
+        this.hotelName = hotelName;
+    }
 }

1ce21b115de070e35417fd896e93cd7badc4332e
216843 Mucha Marcin <56396745+MarcinMucha216843@users.noreply.github.com>
2021-04-28 19:07:41 +0200

    Zmiana na SHA256

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/security/AuthenticationIdentityStore.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/security/AuthenticationIdentityStore.java
@@ -9,8 +9,7 @@ import javax.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
         dataSourceLookup = "java:app/jdbc/ssbd06authDS",
         callerQuery = "SELECT DISTINCT password FROM auth_view WHERE login = ?",
         groupsQuery = "SELECT role FROM auth_view WHERE login = ?",
-        hashAlgorithm = BCryptHash.class,
-        hashAlgorithmParameters = "numRounds=12"
+        hashAlgorithm = ShaHash.class
 )
 public class AuthenticationIdentityStore {
 

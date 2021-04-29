1f41038eed53ff63e0a32c188e1c5d63f5afd767
224304 Paweł Guzek <224304@edu.p.lodz.pl>
2021-04-29 17:01:56 +0200

    Zmiana ShaHash na PasswordHasher; Dodanie @PermitAll do metod rejestracji

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/mok/managers/AccountManager.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/mok/managers/AccountManager.java
@@ -5,9 +5,10 @@ import pl.lodz.p.it.ssbd2021.ssbd06.entities.PendingCode;
 import pl.lodz.p.it.ssbd2021.ssbd06.entities.enums.CodeType;
 import pl.lodz.p.it.ssbd2021.ssbd06.exceptions.AppBaseException;
 import pl.lodz.p.it.ssbd2021.ssbd06.mok.facades.AccountFacade;
-import pl.lodz.p.it.ssbd2021.ssbd06.security.ShaHash;
+import pl.lodz.p.it.ssbd2021.ssbd06.security.PasswordHasher;
 import pl.lodz.p.it.ssbd2021.ssbd06.utils.email.EmailSender;
 
+import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
@@ -46,8 +47,9 @@ public class AccountManager {
      * @param account obiekt encji konta
      * @throws AppBaseException podczas wystąpienia błędu związanego z istniejącym loginem lub problemem z bazą danych.
      */
+    @PermitAll
     public void register(Account account) throws AppBaseException {
-        account.setPassword(new ShaHash().generate(account.getPassword().toCharArray()));
+        account.setPassword(new PasswordHasher().generate(account.getPassword().toCharArray()));
         account.setEnabled(true);
         account.setConfirmed(false);
         account.setCreatedBy(account);

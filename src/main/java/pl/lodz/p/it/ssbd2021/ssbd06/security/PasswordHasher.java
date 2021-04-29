2c1a35974c6c8c682a5dcec2b5e5dd8f6cc0fb88
224322 Karski Dominik <224322@edu.p.lodz.pl>
2021-04-29 14:33:28 +0200

    Dodanie implementacji interfejsu PasswordHash

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/security/PasswordHasher.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/security/PasswordHasher.java
@@ -2,17 +2,34 @@ package pl.lodz.p.it.ssbd2021.ssbd06.security;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import javax.security.enterprise.identitystore.PasswordHash;
+
 /**
  * Klasa umożliwiająca haszowanie algorytmem sha256
  */
-public class PasswordHasher {
+public class PasswordHasher implements PasswordHash {
+
+    @Override
+    public String generate(char[] password) {
+        return DigestUtils.sha256Hex(new String(password));
+    }
+
+    @Override
+    public boolean verify(char[] password, String hashedPassword) {
+        String toVerify = DigestUtils.sha256Hex(new String(password));
+        if(hashedPassword.equals(toVerify)) {
+            return true;
+        }
+        else return false;
+    }
 
     /**
      * Przekształca hasło do postaci niejawnej
      * @param password hasło
      * @return hasło w postaci niejawnej
      */
-    public static String sha256Hash(String password) {
+    public static String generate(String password) {
         return DigestUtils.sha256Hex(password);
     }
+
 }

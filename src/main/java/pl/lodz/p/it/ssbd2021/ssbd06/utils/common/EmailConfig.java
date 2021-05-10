263734b209a0370e177107118bc7040dadc9dc40
224369 Maksajda Michał <224369@edu.p.lodz.pl>
2021-05-10 09:54:12 +0200

    Dodano emailResources_pl oraz EmailSender javadoc

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/utils/common/EmailConfig.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/utils/common/EmailConfig.java
@@ -33,9 +33,9 @@ public class EmailConfig implements Serializable {
 
     @AllArgsConstructor
     public enum MailType {
-        GRANT_ACCESS("grantaccess"), DENY_ACCESS("denyaccess"),
+        GRANT_ACCESS("grant_access"), DENY_ACCESS("deny_access"),
         LOCK_ACCOUNT("lock"), UNLOCK_ACCOUNT("unlock"),
-        RESET_PASSWORD("passwordreset"),
+        RESET_PASSWORD("password_reset"),
         DELETE_UNCONFIRMED("delete"),
         ACTIVATE_ACCOUNT("activate");
 

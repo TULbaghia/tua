e99408e3dac91fc0359f6d0f832a9adbb5e7605b
224369 Maksajda Michał <224369@edu.p.lodz.pl>
2021-05-21 20:31:32 +0200

    Podlaczenie akcji formularza do backendu aplikacji

--- src/main/frontend/src/components/passwordReset/PasswordResetForm.jsx
+++ src/main/frontend/src/components/passwordReset/PasswordResetForm.jsx
@@ -1,67 +1,77 @@
 import {withNamespaces} from "react-i18next";
 import BreadCrumb from "../BreadCrumb";
-import {Link, useParams} from "react-router-dom";
-import React from "react";
+import {Link, Redirect, useParams} from "react-router-dom";
+import React, {useState} from "react";
 import {Form, Formik} from 'formik';
 import FieldComponent from "./FieldComponent";
+import {Configuration, DefaultApi} from "api-client";
 
 function PasswordResetForm({t, i18n}) {
+    const [redirect, setRedirect] = useState(false);
     let {code} = useParams();
+    const conf = new Configuration()
+    const api = new DefaultApi(conf)
 
     return (
-        <div className="container">
-            <BreadCrumb>
-                <li className="breadcrumb-item"><Link to="/">{t('mainPage')}</Link></li>
-                <li className="breadcrumb-item"><Link to="/login">{t('logging')}</Link></li>
-                <li className="breadcrumb-item active" aria-current="page">{t('passwordResetFormTitle')}</li>
-            </BreadCrumb>
+        <>
+            {redirect ? <Redirect to="/" /> : ""}
+            <div className="container">
+                <BreadCrumb>
+                    <li className="breadcrumb-item"><Link to="/">{t('mainPage')}</Link></li>
+                    <li className="breadcrumb-item"><Link to="/login">{t('logging')}</Link></li>
+                    <li className="breadcrumb-item active" aria-current="page">{t('passwordResetFormTitle')}</li>
+                </BreadCrumb>
 
-            <div className="floating-box" style={{minWidth: "30rem"}}>
-                <div>
-                    <h1 className="h3 mb-4">{t('passwordResetFormTitle')}</h1>
-                    <Formik
-                        initialValues={{newPassword: '', repeatedNewPassword: ''}}
-                        validate={values => {
-                            const errors = {};
-                            if (!values.newPassword) {
-                                errors.newPassword = t('passwordRequired');
-                            } else if (values.newPassword.length > 64 || values.newPassword.length < 8) {
-                                errors.newPassword = t('passwordLengthError');
-                            }
+                <div className="floating-box" style={{minWidth: "30rem"}}>
+                    <div>
+                        <h1 className="h3 mb-4">{t('passwordResetFormTitle')}</h1>
+                        <Formik
+                            initialValues={{newPassword: '', repeatedNewPassword: ''}}
+                            validate={values => {
+                                const errors = {};
+                                if (!values.newPassword) {
+                                    errors.newPassword = t('passwordRequired');
+                                } else if (values.newPassword.length > 64 || values.newPassword.length < 8) {
+                                    errors.newPassword = t('passwordLengthError');
+                                }
 
-                            if (!values.repeatedNewPassword) {
-                                errors.repeatedNewPassword = t('repeatedPasswordRequired');
-                            } else if (values.repeatedNewPassword.length > 64 || values.repeatedNewPassword.length < 8) {
-                                errors.repeatedNewPassword = t('passwordLengthError');
-                            }
+                                if (!values.repeatedNewPassword) {
+                                    errors.repeatedNewPassword = t('repeatedPasswordRequired');
+                                } else if (values.repeatedNewPassword.length > 64 || values.repeatedNewPassword.length < 8) {
+                                    errors.repeatedNewPassword = t('passwordLengthError');
+                                }
 
-                            if (!(values.repeatedNewPassword === values.newPassword)) {
-                                errors.repeatedNewPassword = t('passwordsDontMatch');
-                            }
-                            return errors;
-                        }}
-                        onSubmit={(values, {setSubmitting}) => {
-                            console.log(values.newPassword + ", " + values.repeatedNewPassword + ", path param: " + code);
-                            setSubmitting(false);
-                        }}
-                    >
-                        {({isSubmitting, handleChange}) => (
-                            <Form className={{alignItems: "center"}}>
-                                <FieldComponent name="newPassword" placeholder={t('password')}
-                                                handleChange={handleChange}/>
-                                <FieldComponent name="repeatedNewPassword" placeholder={t('repeatPassword')}
-                                                handleChange={handleChange}/>
-                                <button className="btn btn-lg btn-primary btn-block mt-2"
-                                        type="submit" disabled={isSubmitting}
-                                        style={{backgroundColor: "#7749F8", width: "70%", margin: "auto"}}>
-                                    {t('send')}
-                                </button>
-                            </Form>
-                        )}
-                    </Formik>
+                                if (!(values.repeatedNewPassword === values.newPassword)) {
+                                    errors.repeatedNewPassword = t('passwordsDontMatch');
+                                }
+                                return errors;
+                            }}
+                            onSubmit={(values, {setSubmitting}) => {
+                                api.resetPassword({password: values.newPassword, resetCode: code}).then((res) => {
+                                    setRedirect(true);
+                                }).catch(err => {
+                                    console.log("code: " + err.response.status + ", i18n key: " + err.response.data.message);
+                                    setSubmitting(false);
+                                })
+                            }}>
+                            {({isSubmitting, handleChange}) => (
+                                <Form className={{alignItems: "center"}}>
+                                    <FieldComponent name="newPassword" placeholder={t('password')}
+                                                    handleChange={handleChange}/>
+                                    <FieldComponent name="repeatedNewPassword" placeholder={t('repeatPassword')}
+                                                    handleChange={handleChange}/>
+                                    <button className="btn btn-lg btn-primary btn-block mt-2"
+                                            type="submit" disabled={isSubmitting}
+                                            style={{backgroundColor: "#7749F8", width: "70%", margin: "auto"}}>
+                                        {t('send')}
+                                    </button>
+                                </Form>
+                            )}
+                        </Formik>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 

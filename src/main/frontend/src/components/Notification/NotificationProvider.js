d5b89583e2c7ad38ce4f09dd1855e2eaa736cdba
224304 Paweł Guzek <224304@edu.p.lodz.pl>
2021-05-22 14:52:09 +0200

    Poprawiono powiadomienia; Ujednolicono wyglad dla powiadomien; Dodano dialogi z pytaniami; Tłumaczenia

--- src/main/frontend/src/components/Notification/NotificationProvider.js
+++ src/main/frontend/src/components/Notification/NotificationProvider.js
@@ -1,7 +1,8 @@
 import React, {createContext, useContext, useReducer} from 'react';
-import Notification from "./Notification";
+import Notification, {dialogDuration, dialogType} from "./Notification";
 import {v4} from "uuid";
 import './NotificationWrapper.scss'
+import i18n from '../../i18n';
 
 const NotificationContext = createContext();
 
@@ -19,7 +20,7 @@ const NotificationProvider = (props) => {
 
     return (
         <NotificationContext.Provider value={dispatch}>
-            <div className={"notification-wrapper position-fixed"}>
+            <div id={"bottom-notification-wrapper"} className={"notification-wrapper position-fixed"}>
                 {state.map((note) => {
                     return <Notification dispatch={dispatch} key={note.id} {...note} />
                 })}
@@ -29,7 +30,7 @@ const NotificationProvider = (props) => {
     )
 };
 
-export const useNotification = () => {
+export const useNotificationCustom = () => {
     const dispatch = useContext(NotificationContext);
 
     return ({type, duration, message, title, ...props}) => {
@@ -41,6 +42,60 @@ export const useNotification = () => {
                 dialogDuration: duration,
                 message: message,
                 title: title,
+                ...props
+            }
+        })
+    }
+};
+
+export const useNotificationSuccessAndShort = () => {
+    const dispatch = useContext(NotificationContext);
+
+    return ({message, ...props}) => {
+        dispatch({
+            type: "ADD_NOTIFICATION",
+            payload: {
+                id: v4(),
+                dialogType: dialogType.SUCCESS,
+                dialogDuration: dialogDuration.SHORT,
+                title: i18n.t("notification.title.success"),
+                message: message,
+                ...props
+            }
+        })
+    }
+};
+
+export const useNotificationDangerAndLong = () => {
+    const dispatch = useContext(NotificationContext);
+
+    return ({message, ...props}) => {
+        dispatch({
+            type: "ADD_NOTIFICATION",
+            payload: {
+                id: v4(),
+                dialogType: dialogType.DANGER,
+                dialogDuration: dialogDuration.LONG,
+                title: i18n.t("notification.title.danger"),
+                message: message,
+                ...props
+            }
+        })
+    }
+};
+
+export const useNotificationWarningAndLong = () => {
+    const dispatch = useContext(NotificationContext);
+
+    return ({message, ...props}) => {
+        dispatch({
+            type: "ADD_NOTIFICATION",
+            payload: {
+                id: v4(),
+                dialogType: dialogType.WARNING,
+                dialogDuration: dialogDuration.LONG,
+                title: i18n.t("notification.title.warning"),
+                message: message,
                 ...props
             }
         })

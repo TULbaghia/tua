d51135fd6dd468798d946d4bb058d14a9961c7f9
224369 Maksajda Michał <224369@edu.p.lodz.pl>
2021-05-22 13:33:39 +0200

    Dodano obsluge bledow w komponencie EmailConfirm, dodano klucze i18n

--- src/main/frontend/src/components/emailConfirmation/EmailConfirm.jsx
+++ src/main/frontend/src/components/emailConfirmation/EmailConfirm.jsx
@@ -4,8 +4,11 @@ import {Link, useParams} from "react-router-dom";
 import { withNamespaces } from 'react-i18next';
 import BreadCrumb from "../BreadCrumb"
 import {Configuration, DefaultApi} from "api-client";
+import {useNotification} from "../Notification/NotificationProvider";
+import {dialogDuration, dialogType} from "../Notification/Notification";
 
 function EmailConfirm({t,i18n}) {
+    const dispatch = useNotification();
     const history = useHistory();
     let {code} = useParams();
     const conf = new Configuration()
@@ -13,9 +16,20 @@ function EmailConfirm({t,i18n}) {
 
     const handleConfirm = () => {
         api.confirmEmail(code).then((res) => {
+            dispatch({
+                dialogType: dialogType.SUCCESS,
+                dialogDuration: dialogDuration.SHORT,
+                message: t('emailConfirm.success.info'),
+                title: t('operationSuccess')
+            })
             history.push("/login");
         }).catch(err => {
-            console.log("code: " + err.response.status + ", i18n key: " + err.response.data.message);
+            dispatch({
+                dialogType: dialogType.DANGER,
+                dialogDuration: dialogDuration.SHORT,
+                message: t(err.response.data.message),
+                title: t('operationError')
+            })
         })
     }
 
@@ -23,17 +37,17 @@ function EmailConfirm({t,i18n}) {
         <div className="container">
             <BreadCrumb>
                 <li className="breadcrumb-item"><Link to="/">{t('mainPage')}</Link></li>
-                <li className="breadcrumb-item active" aria-current="page">{t('emailVerification')}</li>
+                <li className="breadcrumb-item active" aria-current="page">{t('emailConfirm.title')}</li>
             </BreadCrumb>
             <div className="floating-box">
                 <form className="form-signin p-0">
-                    <h3 className="mb-4">{t('emailVerification')}</h3>
-                    <span>{t('emailVerificationInfo')}</span>
+                    <h3 className="mb-4">{t('emailConfirm.title')}</h3>
+                    <span>{t('emailConfirm.info')}</span>
                     <button className="btn btn-lg btn-primary btn-block mt-5"
                             type="button"
                             onClick={() => handleConfirm()}
                             style={{backgroundColor: "#7749F8", whiteSpace: 'normal'}}>
-                        {t('emailVerificationAction')}
+                        {t('emailConfirm.action')}
                     </button>
                 </form>
             </div>

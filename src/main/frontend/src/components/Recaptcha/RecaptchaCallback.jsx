7e80e93d3060a25323e7be7c9f91b738e7357d32
224438 Szewc Mateusz <224438@edu.p.lodz.pl>
2021-05-26 04:52:05 +0200

    Poprawiono formularz logowania

--- src/main/frontend/src/components/Recaptcha/RecaptchaCallback.jsx
+++ src/main/frontend/src/components/Recaptcha/RecaptchaCallback.jsx
@@ -9,3 +9,13 @@ export const handleRecaptcha = (onSuccessAction, recaptchaRef, dispatchNotificat
         window.grecaptcha.reset();
     }
 }
+
+export const recaptchaCheck = (recaptchaRef, dispatchNotification) => {
+    if (recaptchaRef.current.getValue() === "") {
+        dispatchNotification({message: i18n.t('recaptchaInvalid')});
+        return false;
+    } else {
+        window.grecaptcha.reset();
+        return true;
+    }
+}

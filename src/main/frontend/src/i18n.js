1d8841aefd8763b772d220d8c11f66e0f7c9264a
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-05-29 14:14:23 +0200

    Poprawka wyświetlania godzin, minut i sekund mniejszych niż 10

--- src/main/frontend/src/i18n.js
+++ src/main/frontend/src/i18n.js
@@ -34,7 +34,15 @@ export const dateConverter = (date) =>{
     const parsedDate = new Date(date)
     const day = parsedDate.getDate()
     const year = parsedDate.getFullYear()
-    const time = `${parsedDate.getHours()}:${parsedDate.getMinutes()}:${parsedDate.getSeconds()}`
+    let hours = parsedDate.getHours()
+    let minutes = parsedDate.getMinutes()
+    let seconds = parsedDate.getSeconds()
+
+    if (hours < 10) hours = `0${hours}`
+    if (minutes < 10) minutes = `0${minutes}`
+    if (seconds < 10) seconds = `0${seconds}`
+
+    const time = `${hours}:${minutes}:${seconds}`
     if (i18n.language === 'pl'){
         const month = plMonths[parsedDate.getMonth()]
         const weekDay = plDays[parsedDate.getDay()]

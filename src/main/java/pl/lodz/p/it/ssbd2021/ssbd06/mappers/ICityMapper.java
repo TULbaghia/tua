d3692e438b5240f7aa9d78084426514a626514e1
224438 Szewc Mateusz <224438@edu.p.lodz.pl>
2021-06-11 22:33:36 +0200

    Uzupełniono dokumentacje

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/mappers/ICityMapper.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/mappers/ICityMapper.java
@@ -4,8 +4,16 @@ import org.mapstruct.Mapper;
 import pl.lodz.p.it.ssbd2021.ssbd06.entities.City;
 import pl.lodz.p.it.ssbd2021.ssbd06.moh.dto.CityDto;
 
+/**
+ * Odpowiada za przeprowadzenie mapowania z obiektów klasy City na klasy DTO, oraz z obiektów klas DTO na obiekty klasy City
+ */
 @Mapper
 public interface ICityMapper {
 
+    /**
+     * Dokonuje mapowania z obiektu City na obiekt CityDto
+     * @param city obiekt klasy City
+     * @return zmapowany obiekt klasy CityDto
+     */
     CityDto toCityDto(City city);
 }

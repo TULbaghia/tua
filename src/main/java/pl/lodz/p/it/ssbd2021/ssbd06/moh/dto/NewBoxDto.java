3c65cc90e7888bd24dc01ca1e1b369f6608835a7
224434 Krzysztof Szcześniak <224434@edu.p.lodz.pl>
2021-06-14 15:30:00 +0200

    Dodanie klasy walidujacej typy wyliczeniowe

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/moh/dto/NewBoxDto.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/moh/dto/NewBoxDto.java
@@ -3,6 +3,8 @@ package pl.lodz.p.it.ssbd2021.ssbd06.moh.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import pl.lodz.p.it.ssbd2021.ssbd06.entities.enums.AnimalType;
+import pl.lodz.p.it.ssbd2021.ssbd06.validation.moh.ValueOfEnum;
 
 import java.math.BigDecimal;
 
@@ -14,6 +16,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class NewBoxDto {
     private BigDecimal price;
+    @ValueOfEnum(enumClass = AnimalType.class)
     private String animalType;
     private Long hotelId;
 }

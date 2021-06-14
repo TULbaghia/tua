137639dd6d83bca6d031952441e02566f020aa09
224323 Kasierski Dominik <224323@edu.p.lodz.pl>
2021-06-14 12:38:22 +0200

    Dane dotyczące hotelu wczytywane są z bazy danych

--- src/main/frontend/src/components/HotelInfo/HotelInfo.jsx
+++ src/main/frontend/src/components/HotelInfo/HotelInfo.jsx
@@ -1,57 +1,118 @@
-import React from "react";
+import React, {useState} from "react";
 import cat from "../../images/cat.png"
 import {withNamespaces} from "react-i18next";
 import queryString from "query-string";
 import "../../css/floatingbox.css";
-import {useLocation} from "react-router";
+import {useHistory, useLocation} from "react-router";
 import {ListGroup, Tab, Tabs} from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import RatingComponent from "./RatingComponent";
 import {faCity, faMapMarkedAlt} from '@fortawesome/free-solid-svg-icons'
 import {Rating} from "@material-ui/lab";
+import {api} from "../../Api";
+import {dateConverter} from "../../i18n";
 
 
 function Home(props) {
     const {t, i18n} = props
     const location = useLocation();
+    const history = useHistory();
     const parsedQuery = queryString.parse(location.search);
+    const [hotelData, setHotelData] = useState({
+        address: "",
+        cityName: "",
+        description: "",
+        name: "",
+        rating: "",
+        image: "",
+    });
+
+    const [ratingData, setRatingData] = useState([
+        {
+            id: "",
+            comment: "",
+            createdBy: "",
+            hidden: "",
+            rate: "",
+            creationDate: ""
+        }
+    ]);
+
+    React.useEffect(() => {
+        handleHotelDataFetch();
+    }, []);
+
+    const handleHotelDataFetch = () => {
+        getHotelInfo().then(res => {
+            console.log(res.data);
+            setHotelData(res.data);
+            handleRatingDataFetch();
+        }).catch(err => {
+            if (err.response != null) {
+                if (err.response.status === 500) {
+                    history.push("/errors/internal");
+                }
+            }
+        });
+    }
+
+    const getHotelInfo = async () => {
+        return await api.getHotel(parsedQuery.id);
+    }
+
+    const handleRatingDataFetch = () => {
+        getRatingInfo().then(res => {
+            console.log(res.data);
+            setRatingData({
+                ...res.data,
+                creationDate: dateConverter(res.data.creationDate.slice(0, -5)),
+            });
+            sortRatingData();
+        }).catch(err => {
+            if (err.response != null) {
+                if (err.response.status === 500) {
+                    history.push("/errors/internal");
+                }
+            }
+        });
+    }
+
+    const getRatingInfo = async () => {
+        return await api.getAllRatings(parsedQuery.id);
+    }
+
+    const sortRatingData = () => {
+        ratingData.sort((a, b) => a.id - b.id);
+    }
 
     return (
         <>
             <div id={"hotelInfo"} className={"container mt-2 p-4 mb-5"}>
                 <div className={"row"}>
                     <div className={"col-md-6 col-sm-8 col-10 mb-2"}>
-                        <h2>Nazwa hotelu</h2>
+                        <h2>{hotelData.name}</h2>
                     </div>
                 </div>
                 <div className={"row"}>
                     <div className={"col-md-4 col-sm-6 col-8 mb-3"}>
-                        <img alt="cat" className="img-fluid" src={cat}/>
+                        <img alt="cat" className="img-fluid" src={hotelData.image === undefined ? cat : hotelData.image}/>
                     </div>
                     <div className={"col-md-8 col-sm-6 col-4"}>
                         <Tabs defaultActiveKey="description" transition={false} id="tab">
                             <Tab eventKey="description" title={t('description')}>
-                                <div className={"text-justify mt-2"}>Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit. Vestibulum
-                                    aliquam nisl id tempor fermentum. Aliquam egestas tincidunt orci ut tincidunt. Quisque ac
-                                    placerat dui. In sed tellus vel lectus ultricies ornare a in mi. Curabitur gravida arcu sit
-                                    amet fringilla iaculis. Nulla odio ante, iaculis non feugiat lacinia, varius ac arcu. Sed
-                                    viverra luctus erat. Sed semper erat libero. Ut eget aliquet risus. Nunc nisi sem, egestas eu
-                                    convallis sed, viverra vel lacus. Donec ut neque purus. Integer convallis lacinia nisi. Duis
-                                    condimentum diam justo, eget rhoncus sapien pretium non. Donec leo ligula, suscipit et finibus
-                                    id, eleifend sed est. Aliquam varius facilisis ex, ut pharetra massa. Donec elementum gravida
-                                    sagittis.
+                                <div className={"text-justify mt-2"}>
+                                    {hotelData.description}
                                 </div>
                             </Tab>
                             <Tab eventKey="location" title={t('location')}>
                                 <ListGroup variant={"flush"}>
                                     <ListGroup.Item className={"d-flex align-items-center"}>
                                         <FontAwesomeIcon icon={faCity} size={"2x"}/>
-                                        <span className={"ml-3"}>Miasto</span>
+                                        <span className={"ml-3"}>{hotelData.cityName}</span>
                                     </ListGroup.Item>
                                     <ListGroup.Item className={"d-flex align-items-center"}>
                                         <FontAwesomeIcon icon={faMapMarkedAlt} size={"2x"}/>
-                                        <span className={"ml-3"}>Ulica</span>
+                                        <span className={"ml-3"}>{hotelData.address}</span>
                                     </ListGroup.Item>
                                 </ListGroup>
                             </Tab>
@@ -60,30 +121,26 @@ function Home(props) {
                 </div>
                 <div className={"row"}>
                     <div className={"col-md-4 d-flex align-items-center"}>
-                        <h4 className={"m-0"}>Komentarze</h4>
+                        <h4 className={"m-0"}>{t('comments')}</h4>
                     </div>
                     <div className={"col-md-8 d-flex align-items-center justify-content-end"}>
-                        <span className={"mr-3"}>Średnia ocen:</span>
+                        <span className={"mr-3"}>{t('averageRating')}</span>
                         <Rating
                             size={"large"}
-                            defaultValue={1.6}
+                            defaultValue={0}
+                            value={hotelData.rating}
                             readOnly
                             precision={0.5}
                         />
-                        <span className={"ml-3"}>[1.6]</span>
+                        <span className={"ml-3"}>[{hotelData.rating}]</span>
                     </div>
                 </div>
                 <div className={"row"}>
                     <div className={"col-md-12 mt-3"}>
-                        <RatingComponent rate={5} login={"Tomek666"}
-                                         content={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."}
-                                         date={"15-02-2021"}/>
-                        <RatingComponent rate={3.5} login={"Adam222"}
-                                         content={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."}
-                                         date={"20-03-2020"}/>
-                        <RatingComponent rate={4} login={"Albert997"}
-                                         content={"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."}
-                                         date={"11-08-2021"}/>
+                        {ratingData.map((item) => (
+                            <RatingComponent rate={item.rate} login={item.createdBy} content={item.comment}
+                                                    hidden={item.hidden} date={item.creationDate}/>
+                        ))}
                     </div>
                 </div>
             </div>
--- src/main/frontend/src/components/HotelInfo/RatingComponent.jsx
+++ src/main/frontend/src/components/HotelInfo/RatingComponent.jsx
@@ -3,9 +3,10 @@ import {Rating} from "@material-ui/lab";
 import {Card} from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faEllipsisV} from '@fortawesome/free-solid-svg-icons'
+import i18n from '../../i18n';
 
 
-export default function RatingComponent({rate, login, content, date}) {
+export default function RatingComponent({rate, login, content, date, hidden}) {
     return (
         <>
             <Card className="mb-4">
@@ -18,7 +19,7 @@ export default function RatingComponent({rate, login, content, date}) {
                     /><FontAwesomeIcon className={"float-right mr-2"} icon={faEllipsisV} size={"lg"}/></Card.Header>
                 <Card.Body>
                     <Card.Text className={"text-justify"}>
-                        {content}
+                        {hidden ? i18n.t('hiddenComment') : content}
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer className={"p-2"}><span className={"float-right text-muted"}>{date}</span><span

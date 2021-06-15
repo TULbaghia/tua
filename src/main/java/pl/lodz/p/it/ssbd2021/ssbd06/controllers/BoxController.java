85a85be461fa2a55d301c4b0c560158dada7f518
224434 Krzysztof Szcześniak <224434@edu.p.lodz.pl>
2021-06-15 17:26:57 +0200

    Dodanie brakującego JavaDoc

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/controllers/BoxController.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/controllers/BoxController.java
@@ -53,6 +53,7 @@ public class BoxController extends AbstractController {
     /**
      * Zwraca listę klatek przypisanych do hotelu
      *
+     * @param id identyfikator hotelu
      * @throws AppBaseException podczas błędu związanego z pobieraniem listy klatek
      * @return lista dto klatek przypisanych do hotelu
      */
@@ -64,6 +65,14 @@ public class BoxController extends AbstractController {
         return repeat(() -> boxEndpoint.getAllBoxesInHotel(id), boxEndpoint);
     }
 
+    /**
+     * Zwraca listę klatek przypisanych do hotelu przeznaczonych dla konkretnego typu zwierzęcia
+     *
+     * @param id identyfikator hotelu
+     * @param animalType typ zwierzęcia
+     * @return lista dto klatek przypisanych do hotelu
+     * @throws AppBaseException podczas błędu związanego z pobieraniem listy klatek
+     */
     @GET
     @Path("/all/{id}/{animalType}")
     @RolesAllowed("getAllBoxes")

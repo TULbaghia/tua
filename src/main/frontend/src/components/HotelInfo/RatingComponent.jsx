e9fedcc5bfed80ff860f6aa49c3b32f33a61f392
224464 Zabroń Mateusz <matzab37@gmail.com>
2021-06-16 11:44:44 +0200

    Obsługa eTaga w widoku.

--- src/main/frontend/src/components/HotelInfo/RatingComponent.jsx
+++ src/main/frontend/src/components/HotelInfo/RatingComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Rating} from "@material-ui/lab";
 import {Button, Card} from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
@@ -11,23 +11,38 @@ import {
 } from "../Utils/Notification/NotificationProvider";
 import {ResponseErrorHandler} from "../Validation/ResponseErrorHandler";
 import axios from "axios";
+import {useLocale} from "../LoginContext";
 
 export default function RatingComponent({id, rate, login, content, date, hidden}) {
 
     const dispatchNotificationSuccess = useNotificationSuccessAndShort();
     const dispatchNotificationDanger = useNotificationDangerAndInfinity();
-    // const visible = 'RatingVisibility.VISIBLE';
-    // const unvisible = 'RatingVisibility.HIDDEN';
+    const [etag, setETag] = useState()
+    const {token, currentRole} = useLocale()
+    const [hiddenValue, setHiddenValue] = useState()
+
+    useEffect(() => {
+        setHiddenValue(hidden)
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/resources/ratings/rating/${id}`, {
+            headers: {
+                "Authorization": token,
+            }
+        })
+            .then(res => {
+                setETag(res.headers.etag)
+            })
+    })
 
     const handleChangeVisibilityClick = (id) => {
         axios.patch(`${process.env.REACT_APP_API_BASE_URL}/resources/ratings/changeVisibility/${id}`, {}, {
             headers: {
-                "Authorization": `${localStorage.getItem("token")}`
+                "Authorization": token,
+                "If-Match": etag
             }
         })
-        // api.changeVisibility(id, {headers: {Authorization: localStorage.getItem("token")}})
             .then(() => {
-                dispatchNotificationSuccess({message: 18n.t('change.comment.visibility.success')})
+                setHiddenValue(!hiddenValue)
+                dispatchNotificationSuccess({message: i18n.t('change.comment.visibility.success')})
             })
             .catch(err => {
                 ResponseErrorHandler(err, dispatchNotificationDanger)
@@ -42,14 +57,18 @@ export default function RatingComponent({id, rate, login, content, date, hidden}
                         value={rate}
                         readOnly
                         precision={0.5}
-                    /><Button className="btn-sm float-right mr-2"
-                              onClick={() => handleChangeVisibilityClick(id)}>
-                    {hidden ? i18n.t('show.button') : i18n.t('hide.button')}
-                    </Button>
+                    />
+                    {currentRole === 'ADMIN' ?
+                        <Button className="btn-sm float-right mr-2"
+                                onClick={() => handleChangeVisibilityClick(id)}>
+                            {hiddenValue ? i18n.t('show.button') : i18n.t('hide.button')}
+                        </Button>
+                        : null
+                    }
                     <FontAwesomeIcon className={"float-right mr-2"} icon={faEllipsisV} size={"lg"}/></Card.Header>
                 <Card.Body>
                     <Card.Text className={"text-justify"}>
-                        {hidden ? i18n.t('hiddenComment') : content}
+                        {hiddenValue ? i18n.t('hiddenComment') : content}
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer className={"p-2"}><span className={"float-right text-muted"}>{date}</span><span

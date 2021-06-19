b93dd580a33b15aebea5016e19a7cbb8474b50da
224438 Szewc Mateusz <224438@edu.p.lodz.pl>
2021-06-19 14:41:17 +0200

    Poprawiono zapytanie do dostępnych boxów

--- src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/entities/Box.java
+++ src/main/java/pl/lodz/p/it/ssbd2021/ssbd06/entities/Box.java
@@ -35,16 +35,19 @@ import java.util.Set;
     @NamedQuery(name = "getAvailableBoxesByTypesAndHotelId", query = "SELECT b FROM Box b WHERE " +
             "b.hotel.id = :hotel_id " +
             "AND b.animalType IN :types " +
-            "AND b.id NOT IN (SELECT bl.box.id FROM BookingLine bl WHERE bl.booking.dateTo >= :dateFrom AND bl.booking.dateFrom <= :dateTo)"
+            "AND b.id NOT IN (SELECT bl.box.id FROM BookingLine bl WHERE bl.booking.dateTo >= :dateFrom AND bl.booking.dateFrom <= :dateTo) " +
+            "AND b.delete = false"
             ),
     @NamedQuery(name = "getAvailableBoxesByTypesByHotelIdAndBetween", query = "SELECT b FROM Box b WHERE " +
             "b.hotel.id = :hotel_id " +
-            "AND b.id NOT IN (SELECT bl.box.id FROM BookingLine bl WHERE bl.booking.dateTo >= :dateFrom AND bl.booking.dateFrom <= :dateTo)"
+            "AND b.id NOT IN (SELECT bl.box.id FROM BookingLine bl WHERE bl.booking.dateTo >= :dateFrom AND bl.booking.dateFrom <= :dateTo) " +
+            "AND b.delete = false"
     ),
     @NamedQuery(name = "getAvailableBoxesByIdListAndHotelId", query = "SELECT b FROM Box b WHERE " +
             "b.hotel.id = :hotel_id " +
             "AND b.id NOT IN (SELECT bl.box.id FROM BookingLine bl WHERE bl.booking.dateTo >= :dateFrom AND bl.booking.dateFrom <= :dateTo AND (bl.booking.status = pl.lodz.p.it.ssbd2021.ssbd06.entities.enums.BookingStatus.IN_PROGRESS OR bl.booking.status = pl.lodz.p.it.ssbd2021.ssbd06.entities.enums.BookingStatus.PENDING))  " +
-            "AND b.id IN :boxIdList"
+            "AND b.id IN :boxIdList " +
+            "AND b.delete = false"
     ),
 })
 @NoArgsConstructor

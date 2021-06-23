d51a24ab7fd76a1852542aec1e636476c3bcfe8c
224438 Szewc Mateusz <224438@edu.p.lodz.pl>
2021-06-23 12:55:51 +0200

    Usunięto zbędny komentarz

--- src/main/frontend/src/components/controls/SelectField.jsx
+++ src/main/frontend/src/components/controls/SelectField.jsx
@@ -2,28 +2,6 @@ import React from 'react'
 import Select from 'react-select'
 import { useField, useFormikContext } from "formik";
 
-// export default function SelectField({
-//   options,
-//   field,
-//   form,
-//   styles,
-//   ref
-// }){
-//   const opt = options ? options.find(option => option.value === field.value) : ''
-//   console.log("opt")
-//   console.log(opt)
-//     return (
-//         <Select
-//         options={options}
-//         name={field.name}
-//         value={opt}
-//         onChange={(option) => form.setFieldValue(field.name, option.value)}
-//         onBlur={field.onBlur}
-//         styles={styles}
-//       />
-//     )
-// }
-
 export default function SelectField({ ...props }) {
   const { setFieldValue } = useFormikContext();
   const [field, fieldMeta, fieldHelpers] = useField(props);
